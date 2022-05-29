5384315769ebe21c1c9f19582b9072ab286641e3
Jana <44931863+janalj@users.noreply.github.com>
2022-05-29 02:51:55 -0700

    add more comments

--- perceptron.py
+++ perceptron.py
@@ -1,43 +1,48 @@
+"""
+The perceptron function updates weights every time the prediction and answers don't align. It then adjusts the weights recursively until there's no disagreement between them two.
+The 5 expected arguments for the function are threshold(float), ajustment factor(float), weights(list), examples(3D list), and number of pass(integer).
+The returned result is formmated in Python dictionary as required in output_template.txt (initial arguments values and inputs(list),prediction(boolean), answer(boolean), adjusted_weights(list) for every pass)
+
+"""
+
 def perceptron(threshold, ajustmentFactor,weights,examples,passNum):
-    weightlenth = len(weights)
-    exampleslenth = len(examples)
-    initialweights = weights
-    result = {'init': {'weights': initialweights, 'threshold': threshold, 'adjustment': ajustmentFactor}}
+    weightlength = len(weights)
+    exampleslength = len(examples)
+    result = {'init': {'weights': weights, 'threshold': threshold, 'adjustment': ajustmentFactor}} # initialize result 
 
     for i in range(passNum):
-        result[i+1] = []
-        for j in range(exampleslenth):
-            dic = {} 
-
-            dic['inputs'] = examples[j][1]
+        result[i+1] = []    #initialize pass key
+        for j in range(exampleslength):
+            dic = {}                                      # initialize/ reset dictionary dic
             Sum = 0
-            for num1,num2 in zip(weights,examples[j][1]): # for each input array
-                Sum += num1*num2                         #calculate their Sum
-                                                        #compare Sum with threshold
-            prediction = False                              #prediction represents peceptron's predition 
-            if (Sum <= threshold):
+            for num1,num2 in zip(weights,examples[j][1]): # Calculate each input array's sum
+                Sum += num1*num2                         
+                                                        
+            prediction = False                            #prediction represents peceptron's predition 
+            if (Sum <= threshold):                        #compare Sum with threshold
                 prediction = False
             else:
                 prediction = True
 
-            dic['prediction'] = prediction
-            dic['answer'] = examples[j][0]
-
-            if(examples[j][0] != prediction):
-                new_weights = weights[:]
-                if (examples[j][0]):
-                    for k in range(weightlenth):
+            if(examples[j][0] != prediction):             #if answer and prediction don't agree with each other, increase or decrease the weight accordinly
+                new_weights = weights[:]                  #new_weights serves as a copy of the weights to prevent updating previous adjected_weights values
+                if (examples[j][0]):                      #if the answer is true but the prediction is false, increase the weights by the amount of ajustmentFactor for every 1 input position
+                    for k in range(weightlength):
                         if (examples[j][1][k]):
                             new_weights[k] += ajustmentFactor                  
-                else:
-                    for a in range(weightlenth):
+                else:                                     #if the prediction is true but the answer is false, decrease the weights by the amount of ajustmentFactor for every 1 input position
+                    for a in range(weightlength):
                         if (examples[j][1][a]):
                             new_weights[a] -= ajustmentFactor
-                 
+
+            dic['inputs'] = examples[j][1]                #push the updated input, prediction, answer, adjested-weights to the dictionary dic
+            dic['prediction'] = prediction
+            dic['answer'] = examples[j][0]
             dic['adjusted_weights'] = new_weights
             
-            result[i+1].append(dic)
-            weights = new_weights[:]
+            result[i+1].append(dic)                       #append dic in the value array for the pass key
+            weights = new_weights[:]                      
         
-
     return result    
+
+
